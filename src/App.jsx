e8f063b432902edcd657b80838b5030e9e4f18c5
mrgowtham120703 <gowthammr120703@gmail.com>
2025-09-09 19:06:09 -0700

    Added Hover to sidebar and suggestion

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import Sidebar from './Sidebar.jsx'
+import Sidebar from './Sidebar.jsx';
+import Feed from './Feed.jsx';
+import Suggestions from './Suggestions.jsx';
 
 const App = () => {
   return (
    <div>
     <div className='d-flex vh-100'>
         <div className='w-20'><Sidebar/></div>
-        <div className='w-50 bg-secondary'>feed</div>
-        <div className='w-30'>suggestions</div>
+        <div className='w-50'><Feed/></div>
+        <div className='w-30'><Suggestions/></div>
     </div>
    </div> 
     
